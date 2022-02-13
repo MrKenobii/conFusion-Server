9bc03ab663474ef69f0999c15571322430508e42
MrKenobii <anldygc35@windowslive.com>
2022-02-13 17:19:26 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '474539914058997',
+        clientSecret: '1880627ea65f739bb8e03d9974376932'
+    }
 }
